6c48617651efc81c16d6ff55dbca533e1f31000e
JacobSutcliffe03 <J.Sutcliffe3356@student.leedsbeckett.ac.uk>
2024-04-05 03:25:33 +0100

    rect test + XML
    rectangle test and docs.

--- ASEproject/ASETestProject/UnitTest1.cs
+++ ASEproject/ASETestProject/UnitTest1.cs
@@ -30,3 +30,25 @@ namespace aseTest
             }
             Assert.IsTrue(executedCommands.Any(command => command.Contains("Circle")));
         }
+
+        /// <summary>
+        /// rectangle command tests.
+        /// </summary>
+        [TestMethod]
+        public void Rectangle_Test()
+        {
+
+            Pen pen = new Pen(Color.Black, 1);
+            Canvas canvas = new Canvas(300, 300);
+            string command = "rectangle 30 40";
+            // Executes the rectangle command
+            CommandParser parser = new CommandParser(command, pen, canvas);
+            List<string> executedCommands = canvas.GetExecutedCommands();
+            //Check command has been run and assert True if it has.
+            Console.WriteLine("Executed Commands:");
+            foreach (string executedCommand in executedCommands)
+            {
+                Console.WriteLine(executedCommand);
+            }
+            Assert.IsTrue(executedCommands.Any(command => command.Contains("Rectangle")));
+        }
