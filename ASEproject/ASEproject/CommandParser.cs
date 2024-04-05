8f85bc50d20b11763738815d712f81d1174db093
JacobSutcliffe03 <J.Sutcliffe3356@student.leedsbeckett.ac.uk>
2024-04-05 01:36:45 +0100

    Create Commandparser
    Create command parser + split commands into elements (commands and params).

--- ASEproject/ASEproject/CommandParser.cs
+++ ASEproject/ASEproject/CommandParser.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    internal class CommandParser
+    /// <summary>
+    /// Parses and processes different commands for the project.
+    /// </summary>
+    public class CommandParser
     {
-    }
-}
+        /// <summary>
+        /// Initializes a new instance of the MyCommandParser class.
+        /// </summary>
+        /// <param name="command">The command to be processed.</param>
+        /// <param name="pen">The pen used for drawing.</param>
+        /// <param name="canvas">The canvas where the shapes are drawn.</param>
+        /// 
+        private string lastCommand;
+        public static Dictionary<string, int> variables = new Dictionary<string, int>();
+        public CommandParser(string command, Pen pen, Canvas canvas)
+        {
+
+
+            Console.WriteLine("command: " + command);
+            string[] elems = command.Split(' ');
+            lastCommand = command;
