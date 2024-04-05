8fd7f9ceb5660aa9782c91764c41bee5f2a0de9a
JacobSutcliffe03 <J.Sutcliffe3356@student.leedsbeckett.ac.uk>
2024-04-05 03:24:37 +0100

    Circle test + XML
    circle test and doc.

--- ASEproject/ASETestProject/UnitTest1.cs
+++ ASEproject/ASETestProject/UnitTest1.cs
@@ -1,14 +1,32 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
 using ASEproject;
 
-namespace ASETestProject
+namespace aseTest
 {
     [TestClass]
-    public class UnitTest1
+    public class aseTest
     {
+        /// <summary>
+        /// circle command tests.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void Circle()
+
         {
+            Pen pen = new Pen(Color.Red, 1);
+            Canvas canvas = new Canvas(300, 300);
+            string command = "circle 50";
+            // Execute the circle command.
+            CommandParser parser = new CommandParser(command, pen, canvas);
+            //Check command has been run and assert True if it has.
+            List<string> executedCommands = canvas.GetExecutedCommands();
+            Console.WriteLine("Executed Commands:");
+            foreach (string executedCommand in executedCommands)
+            {
+                Console.WriteLine(executedCommand);
+            }
+            Assert.IsTrue(executedCommands.Any(command => command.Contains("Circle")));
         }
-    }
-}
