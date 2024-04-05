a65b5089a5e62907db2e0f34284dcefbb6d4debd
JacobSutcliffe03 <J.Sutcliffe3356@student.leedsbeckett.ac.uk>
2024-04-05 02:16:24 +0100

    Form1 Manages form elements + Save button function + XML.
    Added saving to txt files and XML doc.

--- ASEproject/ASEproject/Form1.cs
+++ ASEproject/ASEproject/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,70 +11,55 @@ using System.Windows.Forms;
 
 namespace ASEproject
 {
-    public partial class FormWindow : Form
+    public partial class Form1 : Form
     {
+        Pen pen;
+        Canvas canvas;
+        String command;
+
         /// <summary>
-        /// Bitmap to display in picturebox
+        /// Constructor for form window.
         /// </summary>
-        Bitmap OutputBitmap;
-        Canvas MyCanvas;
-        
-        /// <summary>
-        /// Initialise widgets on form.
-        /// </summary>
-        public FormWindow()
-        {
-            InitializeComponent();
-            OutputBitmap = new Bitmap(OutputPB.Width, OutputPB.Height);
-            MyCanvas = new Canvas(Graphics.FromImage(OutputBitmap));
-        }
 
-        private void SyntaxBTN_Click(object sender, EventArgs e)
+        public Form1()
         {
+            InitializeComponent(); // Initializes the form.
+            pen = new Pen(Color.Red, 5); // Initializes a Pen.
+            canvas = new Canvas(300, 350); // Initializes the canvas.
 
         }
 
-        private void OpenBTN_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void SaveBTN_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
         /// <summary>
-        /// Displays contents of bitmap onto picture box widget.
+        /// Save the programs to txt file.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OutputPB_Paint(object sender, PaintEventArgs e)
+        public void SaveBTN_Click(object sender, EventArgs e)
         {
-            Graphics g = e.Graphics;
-            g.DrawImageUnscaled(OutputBitmap, 0, 0);
-        }
-
-        /// <summary>
-        /// Iteraterativly calls parsecommand on lines in textbox widgets.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void RunBTN_Click(object sender, EventArgs e)
-        {
-            try
+            // Creating a save file.
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
             {
-                for (int i = 0; i < ProgramTB.Lines.Length; i++)
+                string programText = string.Empty;
+                saveFileDialog.Filter = "Text|*.txt|All Files|*.*"; // Filters file type.
+
+                // Null Check
+                if (!string.IsNullOrWhiteSpace(ProgramTB.Text))
                 {
-                   MyCanvas.ParseCommand(ProgramTB.Lines[i]);
+                    programText = ProgramTB.Text; // Assigns the content of the program textbox
+                    saveFileDialog.ShowDialog();  // Shows the dialog for saving the file
+                    string filePath = saveFileDialog.FileName; // Gets the file path
+
+                    if (!string.IsNullOrEmpty(filePath)) // If a file path is provided
+                    {
+                        // Writes the content of the program textbox to the selected file
+                        System.IO.File.WriteAllText(filePath, programText);
+                        MessageBox.Show("Program saved.", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        // Save confirmation message
+                    }
                 }
-                MyCanvas.ParseCommand(CommandLineTB.Text);
             }
-            catch (Exception error)
-            {
-                Console.WriteLine(error.Message);
-            }
-            Refresh();
         }
-    }
-}
