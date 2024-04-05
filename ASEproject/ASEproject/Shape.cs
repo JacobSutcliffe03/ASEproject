4e13a943d737e511e8010dcf76233f5bb6cc2bdd
JacobSutcliffe03 <J.Sutcliffe3356@student.leedsbeckett.ac.uk>
2024-04-05 01:11:58 +0100

    Shape Parent class
    shape class created with shared variables. Form.Designer.cs removed redundant code.

--- ASEproject/ASEproject/Shape.cs
+++ ASEproject/ASEproject/Shape.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    internal class Shape
+    //Shape Class to be inherited from.
+    public abstract class Shape
     {
+        protected Color Colour;
+        protected int XPos, YPos;
+
+        public abstract void DrawShape(Graphics g, Point point);
+        public Shape(Color colour, int x, int y)
+        {
+            this.Colour = colour;
+            this.XPos = x; this.YPos = y;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + XPos + " " + YPos;
+        }
     }
 }
